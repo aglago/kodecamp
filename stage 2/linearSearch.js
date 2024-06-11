ba84268a624bfbb4e446fe4f9dfc21dc81057c42
amimanye <amimanyeaglago@gmail.com>
2024-06-11 07:22:22 +0000

    search for sa string in an array of strings

---
+++ stage 2/linearSearch.js
@@ -0,0 +1,14 @@
+/**
+ * linearSearch - searches for a string in an array of strings
+ * @array: The array of strings to search
+ * @string: the string that we are searching for
+ * @return: string indicating whether the search was successful
+ */
+
+function linearSearch(array, string) { 
+    for (let i = 0; i < array.length; i++) {
+        if (string === array[i])
+            return "Yes, the string exists in the array";
+    }
+    return "No, the string does not exist in the array";
+}
