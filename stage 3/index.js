cb9fe8e7a1d96c6ed929709e576bfc2e91f318b4
kennyAnyi9 <kennyanyi@gmail.com>
2024-06-21 09:07:10 +0000

    added tests and response

--- stage 3/index.js
+++ stage 3/index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import { v4 as uuid } from 'uuid';
+import express from "express";
+import { v4 as uuid } from "uuid";
 
 const app = express();
 
@@ -8,57 +8,66 @@ let tasks = [];
 app.use(express.json());
 
 // A get endpoint to get the home route
-app.get('/', (req, res) => {
-  res.end('this is the home route');
+app.get("/", (req, res) => {
+  res.send("this is the home route");
 });
 
 // A get endpoint to get a list of all tasks.
-app.get('/tasks', (req, res) => {
-  res.end(tasks);
+app.get("/tasks", (req, res) => {
+  res.send(tasks);
 });
 
 // A post endpoint to add a new task.
-app.post('/tasks', (req, res) => {
+app.post("/tasks", (req, res) => {
   const id = uuid();
 
   tasks.push({
     id,
     title: req.body.title,
     body: req.body.body,
-    status: req.body.status
+    status: req.body.status,
   });
+
+  res.send("Task added successfully");
 });
 
 // A get endpoint to get a task by it's ID
-app.get('/tasks/:id', (req, res) => {
+app.get("/tasks/:id", (req, res) => {
   const taskId = req.params.id;
 
   const task = tasks.find((task) => task.id == taskId);
-  res.end(task);
+  res.send(task);
 });
 
 // A put endpoint to change the title and body of a task
-app.put('/tasks/:id', (req, res) => {
+app.put("/tasks/:id", (req, res) => {
   const taskId = req.params.id;
-  const task = tasks.find(task => task.id == taskId);
+  const task = tasks.find((task) => task.id == taskId);
   const { newTitle, newBody } = req.body;
 
   task.title = newTitle;
   task.body = newBody;
+
+  res.send("Task updated successfully");
 });
 
 // A patch endpoint to change the status of a task
-app.patch('/tasks/:id', (req, res) => {
-  const tasdId = req.params.id;
-  const task = tasks.find(task => task.id == taskId);
+app.patch("/tasks/:id", (req, res) => {
+  const taskId = req.params.id;
+  const task = tasks.find((task) => task.id == taskId);
   const { newStatus } = req.body;
 
   task.status = newStatus;
 });
 
 // A delete endpoint to remove a task from the array of tasks.
-app.patch('/tasks/:id', (req, res) => {
-  const tasdId = req.params.id;
+app.patch("/tasks/:id", (req, res) => {
+  const taskId = req.params.id;
 
   tasks = tasks.filter((task) => task.id !== taskId);
-}
+});
+
+// server to listen for request
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
