bae141d97872ad920be396ff656603c60c849da6
amimanye <amimanyeaglago@gmail.com>
2024-07-05 19:15:37 +0000

    feat: sign register in

---
+++ stage 4/controllers/login.controller.js
@@ -0,0 +1,36 @@
+import User from "../models/user.models.js";
+import bcrypt from "bcryptjs";
+import generateJWT from "../utils/generateJWT.js";
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // check if user exists and password is correct
+        const user = await User.findOne({ email });
+        if (!user)
+          return res.status(404).send({ message: "User does not exist" });
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid)
+          return res.status(401).send({ message: "Invalid password" });
+
+        // generate jwt token
+        const jwt = generateJWT({ id: user._id, email });
+
+        // send response
+        res.send({
+          user: {
+            id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+          },
+          token: jwt,
+        });
+    } catch (error) {
+        console.log("Error in login controller:", error.message);
+    }
+
+};
+
+export default login;
--- stage 4/routes/auth.routes.js
+++ stage 4/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import register from "../controllers/register.js";
+import register from "../controllers/register.controller.js";
+import login from "../controllers/login.controller.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post('/register', register);
+authRoutes.post('/login', login);
 
 export default authRoutes
